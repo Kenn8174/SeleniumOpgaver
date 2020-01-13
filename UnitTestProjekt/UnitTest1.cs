0d6c2ed25b3c5412e51d19e6e4f21b4658234fee
Kenneth Jessen <kennethjessen1@hotmail.com>
2020-01-13 14:47:50 +0100

    Næsten alle unittests

--- UnitTestProjekt/UnitTest1.cs
+++ UnitTestProjekt/UnitTest1.cs
@@ -2,6 +2,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SeleniumOpgaver;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UnitTestProjekt
 {
@@ -19,7 +21,7 @@ namespace UnitTestProjekt
         public void Exercise_1_CheckIfTitleIsMoreThanZero()
         {
             // ARANGE
-             string url = "http://shop.demoqa.com/";
+            string url = "http://shop.demoqa.com/";
 
             // ACT
             chromeDriver.Url = url;
@@ -78,8 +80,8 @@ namespace UnitTestProjekt
             string url = "http://shop.demoqa.com/";
 
             // ACT
-            chromeDriver.Url = url; 
-            
+            chromeDriver.Url = url;
+
             chromeDriver.FindElement(By.ClassName("cart-button")).Click();
 
             chromeDriver.Navigate().Back();
@@ -104,7 +106,7 @@ namespace UnitTestProjekt
         #region Practice Exercise – Til afsnit 4
 
         [TestMethod]
-        public void Exercise_4_Del1_CheckIfSubmitWorks()
+        public void Exercise_3_CheckIfSubmitWorks()
         {
             // ARANGE
             string url = "http://toolsqa.com/automation-practice-form/";
@@ -112,7 +114,7 @@ namespace UnitTestProjekt
 
             // ACT
             chromeDriver.Url = url;
-            
+
             chromeDriver.FindElement(By.Id("cookie_action_close_header")).Click();
 
             chromeDriver.FindElement(By.Name("firstname")).SendKeys("Kenneth");
@@ -130,11 +132,11 @@ namespace UnitTestProjekt
         }
 
         [TestMethod]
-        public void Exercise_4_Del2_CheckIfTagNameCanBeFound()
+        public void Exercise_3_CheckIfTagNameCanBeFound()
         {
             // ARANGE
             string url = "http://toolsqa.com/automation-practice-form/";
-            string actual = "Button";
+            string actual = "Simple Button";
 
             // ACT
             chromeDriver.Url = url;
@@ -152,6 +154,98 @@ namespace UnitTestProjekt
             // ASSERT
             Assert.AreEqual(tagNameSubmit, actual);
         }
+
+        #endregion
+
+
+        #region Practice Exercise – Til afsnit Forms Del 1
+
+        [TestMethod]
+        public void Exercise_4_1_CheckIfRadioButtonSelectIsSecond()
+        {
+            // ARANGE
+            string url = "http://toolsqa.com/automation-practice-form/";
+
+            // ACT
+            chromeDriver.Url = url;
+
+            chromeDriver.FindElement(By.Id("cookie_action_close_header")).Click();
+
+            IList<IWebElement> radioSex = chromeDriver.FindElements(By.Name("sex"));
+
+            radioSex.ElementAt(0).Click();
+
+            bool radioValue = radioSex.ElementAt(0).Selected;
+
+            if (radioValue == true)
+            {
+                radioSex.ElementAt(1).Click();
+            }
+            else
+            {
+                radioSex.ElementAt(0).Click();
+            }
+
+            bool expected = radioSex.ElementAt(1).Selected;
+
+            chromeDriver.Close();
+
+            // ASSERT
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod]
+        public void Exercise_4_2_CheckIfRadioButtonSelectIsCorrect()
+        {
+            // ARANGE
+            string url = "http://toolsqa.com/automation-practice-form/";
+
+            // ACT
+            chromeDriver.Url = url;
+            chromeDriver.FindElement(By.Id("cookie_action_close_header")).Click();
+
+            chromeDriver.FindElement(By.Id("exp-2")).Click();
+
+            bool expected = chromeDriver.FindElement(By.Id("exp-2")).Selected;
+
+            chromeDriver.Close();
+
+            // ASSERT
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod]
+        public void Exercise_4_2_CheckIfCheckBoxProfessionIsCorrect()
+        {
+            // ARANGE
+            string url = "http://toolsqa.com/automation-practice-form/";
+
+            // ACT
+            chromeDriver.Url = url;
+            chromeDriver.FindElement(By.Id("cookie_action_close_header")).Click();
+            IList<IWebElement> checkboxProfession = chromeDriver.FindElements(By.Name("profession"));
+
+            int profSize = checkboxProfession.Count();
+
+            for (int i = 0; i < profSize; i++)
+            {
+                string value = checkboxProfession.ElementAt(i).GetAttribute("value");
+
+                if (value.Equals("Automation Tester"))
+                {
+                    checkboxProfession.ElementAt(i).Click();
+                    break;
+                }
+            }
+
+            bool expected = checkboxProfession.ElementAt(1).Selected;
+
+            chromeDriver.Close();
+
+            // ASSERT
+            Assert.IsTrue(expected);
+        }
+
 
         #endregion
 
